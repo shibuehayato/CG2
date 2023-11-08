b4ee2b5fd7852e48dc5ef030baa1e0309bca2b30
shibuehayato <115055046+shibuehayato@users.noreply.github.com>
2023-11-08 12:31:50 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -672,19 +672,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	vertexData[0].position = { -0.5f, -0.5f, 0.0f, 1.0f };
 	vertexData[0].texcoord = { 0.0f, 1.0f };
 	// 上
-	vertexData[1].position = { 0.0f, 0.5f, 0.0f, 1.0f };
-	vertexData[1].texcoord = { 0.5f, 0.0f };
+	vertexData[1].position = { -0.5f, 0.5f, 0.0f, 1.0f };
+	vertexData[1].texcoord = { 0.0f, 0.0f };
 	// 右下
 	vertexData[2].position = { 0.5f, -0.5f, 0.0f, 1.0f };
 	vertexData[2].texcoord = { 1.0f,1.0f };
 	// 左下
-	vertexData[3].position = { 0.0f, 0.5f, 0.0f, 1.0f };
-	vertexData[3].texcoord = { 0.0f, 1.0f };
+	vertexData[3].position = { -0.5f, 0.5f, 0.0f, 1.0f };
+	vertexData[3].texcoord = { 0.0f, 0.0f };
 	// 上
-	vertexData[4].position = { 0.5f, -0.5f, 0.0f, 1.0f };
-	vertexData[4].texcoord = { 0.5f,0.0f };
+	vertexData[4].position = { 0.5f, 0.5f, 0.0f, 1.0f };
+	vertexData[4].texcoord = { 1.0f,0.0f };
 	// 右下
-	vertexData[5].position = { 1.0f, 0.5f, 0.0f, 1.0f };
+	vertexData[5].position = { 0.5f, -0.5f, 0.0f, 1.0f };
 	vertexData[5].texcoord = { 1.0f,1.0f };
 
 	// ビューポート
